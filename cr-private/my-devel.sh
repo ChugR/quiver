afafcf12653b46890f9adbf7c4f77610e19a3dad
Chug <chug@apache.org>
2020-07-23 09:51:00 -0400

    cr's working notes

---
+++ cr-private/my-devel.sh
@@ -0,0 +1,9 @@
+export LIBRARY_PATH=/opt/local/lib64
+export C_INCLUDE_PATH=/opt/local/include
+export CPLUS_INCLUDE_PATH=/opt/local/include
+export LD_LIBRARY_PATH=/opt/local/lib64
+
+export QUIVER_HOME=$PWD/build/quiver
+export PATH=$PWD/build/bin:$PWD/scripts:$PATH
+export PYTHONPATH=$QUIVER_HOME/python:$PWD/python:$PYTHONPATH
+export NODE_PATH=/usr/lib/node_modules:$NODE_PATH
